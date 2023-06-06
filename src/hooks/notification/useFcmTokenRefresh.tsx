62c65fb86e1bef91ef33d3acd21795ef2b2f3101
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-06-06 14:36:19 +0300

    refresh fcm token listener

--- src/hooks/notification/useFcmTokenRefresh.tsx
+++ src/hooks/notification/useFcmTokenRefresh.tsx
@@ -1,22 +1,29 @@
 import messaging from '@react-native-firebase/messaging';
 import {useEffect} from 'react';
-import {useSetFcmTokenMutation, userApi} from '../../app/api/userApi';
+import {userApi} from '../../app/api/userApi';
 import {store} from '../../app/store';
-// import {useDispatch} from 'react-redux';
+import {setFcm} from '../../app/Reducers/User/userSlice';
 
 const useFcmTokenRefresh = () => {
-  // const [setFcmToken, {isLoading, isError}] = store.;
+  const isSignIn = store.getState().reducer.user.isSignIn;
+
   const listenToFCMTokenRefresh = () => {
     messaging().onTokenRefresh(async newToken => {
-      await store.dispatch(userApi.endpoints?.setFcmToken?.initiate(newToken));
+      const oldToken = store.getState().reducer.user.fcmToken;
+      await store.dispatch(
+        userApi.endpoints?.refreshFcmToken?.initiate({newToken, oldToken}),
+      );
     });
   };
 
   const getFCMToken = async () => {
+    const oldToken = store.getState().reducer.user.fcmToken;
+    if (oldToken) return;
     messaging()
       .getToken()
       .then(async token => {
         await store.dispatch(userApi.endpoints?.setFcmToken?.initiate(token));
+        store.dispatch(setFcm(token));
       })
       .catch(err => {
         console.log('Unable to retrieve token', err);
@@ -24,8 +31,10 @@ const useFcmTokenRefresh = () => {
   };
 
   useEffect(() => {
+    if (!isSignIn) return;
     getFCMToken();
-  }, []);
+    listenToFCMTokenRefresh();
+  }, [isSignIn]);
 };
 
 export default useFcmTokenRefresh;
